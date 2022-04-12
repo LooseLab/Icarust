65404895300b11e0bc704206918567c8ddf0960c
adoni5 <roryjmunro1@gmail.com>
2022-04-12 14:39:09 +0100

    Setup server with all relevant services, update gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,91 @@
 #![deny(missing_docs)]
 #![deny(missing_doc_code_examples)]
-
 //! # Icarust
 //!
 //! A binary for running a mock, read-fish compatible grpc server to test live unblocking read-until experiments.
+//! 
+//! Simply `cargo run` in the directory to start the server, which hosts the Manager Server on 127.0.0.1:10000
+//! 
+//! Has one position, which is hosted on 127.0.0.1:10001
 //!  
+mod impl_services;
+#[macro_use]
+extern crate log;
+
 /// Import all our definied services
 mod services;
-
-use crate::services::device::device::device_service_server::DeviceServiceServer;
-use crate::services::device::Device;
-
-use crate::services::instance::instance::instance_service_server::InstanceServiceServer;
-use crate::services::instance::Instance;
-
-use crate::services::analysis::analysis::analysis_configuration_service_server::AnalysisConfigurationServiceServer;
-use crate::services::analysis::Analysis;
-
-use crate::services::log::Log;
-use crate::services::log::log::log_service_server::LogServiceServer;
-
-use crate::services::manager::Manager;
-use crate::services::manager::manager::manager_service_server::ManagerServiceServer;
-
-use crate::services::manager::manager::FlowCellPosition;
-use crate::services::manager::manager::flow_cell_position::{RpcPorts, SharedHardwareGroup, Location};
-
+use std::fs;
 use tonic::transport::Server;
+use toml;
+use serde::Deserialize;
 
+use crate::impl_services::device::Device;
+use crate::impl_services::analysis_configuration::Analysis;
+use crate::impl_services::instance::Instance;
+use crate::impl_services::log::Log;
+use crate::impl_services::manager::Manager;
+use crate::impl_services::acquisition::Acquisition;
+use crate::impl_services::protocol::Protocol;
+use crate::impl_services::data::DataServiceServicer;
+
+use crate::services::minknow_api::device::device_service_server::DeviceServiceServer;
+use crate::services::minknow_api::analysis_configuration::analysis_configuration_service_server::AnalysisConfigurationServiceServer;
+use crate::services::minknow_api::instance::instance_service_server::InstanceServiceServer;
+use crate::services::minknow_api::log::log_service_server::LogServiceServer;
+use crate::services::minknow_api::manager::flow_cell_position::{
+    Location, RpcPorts, SharedHardwareGroup,
+};
+use crate::services::minknow_api::manager::manager_service_server::ManagerServiceServer;
+use crate::services::minknow_api::manager::FlowCellPosition;
+use crate::services::minknow_api::acquisition::acquisition_service_server::AcquisitionServiceServer;
+use crate::services::minknow_api::protocol::protocol_service_server::ProtocolServiceServer;
+use crate::services::minknow_api::data::data_service_server::DataServiceServer;
+
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    sample_name: String,
+    experiment_name: String,
+    flowcell_name: String,
+}
+
+
+/// Loads our config TOML to get the sample name, experiment name and flowcell name, which is returned as a Config struct.
+fn _load_toml(file_path: &str) -> Config {
+    let contents = fs::read_to_string(file_path)
+        .expect("Something went wrong with reading the file.");
+    let config: Config = toml::from_str(&contents).unwrap();
+    return config
+}
 
 /// Main function - Runs two asynchronous GRPC servers
 /// The first server is the manager server, which here manages available sequencing positions and minknow version information.
-/// Once a client connects to the manager it may then connect to the second GRPC server, which manages all the services relating to the 
-/// sequencing position. 
+/// Once a client connects to the manager it may then connect to the second GRPC server, which manages all the services relating to the
+/// sequencing position.
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config = _load_toml("config.toml");
+    println!("{:#?}", config);
+
     let addr_manager = "127.0.0.1:10000".parse().unwrap();
     let addr_position = "127.0.0.1:10001".parse().unwrap();
 
     let manager_init = Manager {
-        positions : [FlowCellPosition {
+        positions: [FlowCellPosition {
             name: String::from("Bantersaurus"),
             state: 1,
-            rpc_ports: Some(RpcPorts{secure:8000, insecure:10001, secure_grpc_web:0, insecure_grpc_web:0}),
-            location: Some(Location{x:1, y:1}),
+            rpc_ports: Some(RpcPorts {
+                secure: 8000,
+                insecure: 10001,
+                secure_grpc_web: 0,
+                insecure_grpc_web: 0,
+            }),
+            location: Some(Location { x: 1, y: 1 }),
             error_info: "Unknown state, please help".to_string(),
-            shared_hardware_group: Some(SharedHardwareGroup{group_id: 1}),
+            shared_hardware_group: Some(SharedHardwareGroup { group_id: 1 }),
             is_integrated: true,
-            can_sequence_offline: true
-        }]
+            can_sequence_offline: true,
+        }],
     };
     // Create the manager server and add the ervice to it
     let svc = ManagerServiceServer::new(manager_init);
@@ -60,13 +97,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             .await
             .unwrap();
     });
-    println!("Hello!");
     // Create the position server for our one position.
     let log_svc = LogServiceServer::new(Log {});
-    let insta_svc = InstanceServiceServer::new(Instance{});
-    let anal_svc = AnalysisConfigurationServiceServer::new(Analysis{});
-    let devi_svc = DeviceServiceServer::new(Device{});
-    
-    Server::builder().add_service(log_svc).add_service(devi_svc).add_service(insta_svc).add_service(anal_svc).serve(addr_position).await?;
+    let insta_svc = InstanceServiceServer::new(Instance {});
+    let anal_svc = AnalysisConfigurationServiceServer::new(Analysis {});
+    let devi_svc = DeviceServiceServer::new(Device {});
+    let acquisition_svc = AcquisitionServiceServer::new(Acquisition {});
+    let protocol_svc = ProtocolServiceServer::new(Protocol{});
+    let data_svc = DataServiceServer::new(DataServiceServicer::new(3000));
+
+
+    Server::builder()
+        .add_service(log_svc)
+        .add_service(devi_svc)
+        .add_service(insta_svc)
+        .add_service(anal_svc)
+        .add_service(acquisition_svc)
+        .add_service(protocol_svc)
+        .add_service(data_svc)
+        .serve(addr_position)
+        .await?;
     Ok(())
-} 
+}
