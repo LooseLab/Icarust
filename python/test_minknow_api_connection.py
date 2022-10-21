352bc20cf35de69d5bc5cd827adab5e09a0bc372
Adoni5 <roryjmunro1@gmail.com>
2022-10-21 13:58:18 +0100

    Update to use an existing end point

--- python/test_minknow_api_connection.py
+++ python/test_minknow_api_connection.py
@@ -1,7 +1,13 @@
-import logging 
+from distutils.command.build_scripts import first_line_re
+import logging
+import grpc
+from matplotlib.pyplot import minorticks_off 
 from rich.logging import RichHandler
 from minknow_api.manager import Manager
 from minknow_api import __version__ as mversion
+from pathlib import Path
+import minknow_api
+
 
 logger = logging.getLogger(__name__)
 logger.addHandler(RichHandler())
@@ -29,12 +35,15 @@ class MinionManager(Manager):
         use_tls: bool
             Default False. Use TLS connection
         """
+        print(Path().cwd())
+        print(minknow_api.GRPC_CHANNEL_OPTIONS)
         logger.info(f"Installed minknow API Version {mversion}")
         if mversion.startswith("5"):
             host = kwargs.get("host", "localhost")
             port = kwargs.get("port", 10000)
             super().__init__(host, port)
         elif mversion[:2] in {"4.2", "4.3", "4.4", "4.5"}:
+            print("Version is less than 5.")
             host = kwargs.get("host", "localhost")
             port = kwargs.get("port", 9501)
             super().__init__(host, port, use_tls=False)
@@ -56,7 +65,7 @@ class MinionManager(Manager):
                 running = "yes" if position.running else "no"
                 if position.running:
                     connected_position = position.connect()
-                    device_connected = connected_position.device.get_device_state().device_state
+                    device_connected = connected_position.device.get_calibration(first_channel=1, last_channel=512)
                     if device_connected:
                         # TODO note that we cannot connect to a remote instance without an ip websocket
                         logger.info("Connected")
